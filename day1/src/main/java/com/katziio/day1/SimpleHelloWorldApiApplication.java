a0b8eeda7f0873b26563c77b523bebd8cb133177
Vignesh <vignesh000129@gmail.com>
2023-08-18 19:22:41 +0530

    Hello world api setup

---
+++ day1/src/main/java/com/katziio/day1/SimpleHelloWorldApiApplication.java
@@ -0,0 +1,13 @@
+package com.katziio.day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHelloWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHelloWorldApiApplication.class, args);
+	}
+
+}
