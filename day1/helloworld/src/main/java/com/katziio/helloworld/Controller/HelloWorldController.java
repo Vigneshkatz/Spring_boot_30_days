554fa0b6cfa8ac0962cea324353150a9d1b2f50d
Vignesh <vignesh000129@gmail.com>
2023-08-18 23:05:52 +0530

    Added HelloWorldController.java added

--- day1/helloworld/src/main/java/com/katziio/helloworld/Controller/HelloWorldController.java
+++ day1/helloworld/src/main/java/com/katziio/helloworld/Controller/HelloWorldController.java
@@ -1,8 +1,10 @@
 package com.katziio.helloworld.Controller;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String HelloWorld(){
+        return "Hello World";
+    }
 }
