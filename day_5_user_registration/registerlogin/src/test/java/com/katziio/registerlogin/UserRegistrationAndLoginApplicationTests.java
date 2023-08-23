23cf2ed42cac7d8ec519b906106bb0944429cf31
Vignesh <Vignesh@apples-MacBook-Pro.local>
2023-08-23 15:28:36 +0530

    SetUp for user registration and login

---
+++ day_5_user_registration/registerlogin/src/test/java/com/katziio/registerlogin/UserRegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katziio.registerlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
