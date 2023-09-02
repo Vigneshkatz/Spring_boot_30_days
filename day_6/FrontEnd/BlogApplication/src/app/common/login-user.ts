e3ceff11ed8f0fae9647a4d025759db3b2ccc723
Vignesh <vignesh000129@gmail.com>
2023-09-02 11:21:08 +0530

    Remote file from old branch

--- day_6/FrontEnd/BlogApplication/src/app/common/login-user.ts
+++
@@ -1,9 +0,0 @@
-export class LoginUser {
-  public password: string;
-  public email: string;
-
-  constructor(email: string, password: string) {
-    this.email = email;
-    this.password = password;
-  }
-}
--- day_6/jpamapping/src/main/java/com/katziio/jpamapping/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.katziio.jpamapping.repository;
-
-import com.katziio.jpamapping.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
